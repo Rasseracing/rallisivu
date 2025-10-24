a162a6c1903cad364019fbe936f6d6286795a95e
Rasseracing <rasmus.o.pentti@gmail.com>
2025-10-24 19:31:17 +0300

    ensimmainen

---
+++ src/lib/component/Tiedot.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let { nimi } = $props();
+    let arvo = $state("rasse");
+</script>
+
+<h1>minun nimeni on {arvo}</h1>
+
+<input type="text" bind:value={arvo} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Tiedot from "$lib/component/Tiedot.svelte";
+
+ 
+
+</script>
+<h1>Hei olen Rasmus ja ajan rallia</h1>
+
+<Tiedot nimi="rasse">
+   
+
+</Tiedot>
